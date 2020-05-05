124314f64a70bd6463d196f27160721dd8c05650
lixiang093214 <lixianggxu@163.com>
2020-05-05 19:45:20 +0800

    add testcase of checking number is multiple of 5.

--- src/test/java/cn/xpbootcamp/fizzbuzz/CountOffTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/CountOffTest.java
@@ -6,8 +6,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CountOffTest {
 
     @Test
-  public void should_say_nothing_for_multiple_of_3(){
+    public void should_say_nothing_for_multiple_of_3(){
         assertThat("Fizz").isEqualTo(new CountOff().checkCountOff(3));
+    }
+
+    @Test
+    public void should_say_nothing_for_multiple_of_5(){
+        assertThat("Buzz").isEqualTo(new CountOff().checkCountOff(5));
     }
 
 
