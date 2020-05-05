9b3c05efa8c50d7b80e604d29cca4a2b77e0ba27
lixiang093214 <lixianggxu@163.com>
2020-05-05 19:33:40 +0800

    add test class and main class for count off.

---
+++ src/main/java/cn/xpbootcamp/fizzbuzz/CountOff.java
@@ -0,0 +1,11 @@
+package cn.xpbootcamp.fizzbuzz;
+
+public class CountOff {
+
+   public String checkCountOff(int number){
+       String resultStr="";
+
+       return resultStr;
+
+   }
+}
---
+++ src/test/java/cn/xpbootcamp/fizzbuzz/CountOffTest.java
@@ -0,0 +1,12 @@
+package cn.xpbootcamp.fizzbuzz;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CountOffTest {
+
+    @Test
+  public void should_say_nothing_for_multiple_of_3(){
+
+    }
+}
